c82f985a3096c662e86a9e5fa5acfff6a2383999
Taz Uddin <getleton@gmail.com>
2020-08-18 00:36:38 +0600

    Add repository

---
+++ SpecificationPattern/Repositories/MovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using SpecificationPattern.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecificationPattern.Repositories
+{
+    public class MovieRepository 
+    {
+        private readonly MoviesContext _context;
+        public MovieRepository()
+        {
+            _context = new MoviesContext();
+        }
+
+
+
+
+    }
+}
