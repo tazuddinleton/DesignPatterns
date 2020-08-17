a47556af084066d66e901bf6a03d1b3ef59a9288
Taz Uddin <getleton@gmail.com>
2020-08-18 00:35:51 +0600

    Add ValueObject

---
+++ SpecificationPattern/ValueObjects/MpaaRating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecificationPattern.ValueObjects
+{
+    public enum MpaaRating
+    {
+        G = 1,
+        PG = 2,
+        PG13 = 3,
+        R = 4
+    }
+}
