11e7d4978ed38feeac500c29f77260225b9b7cd9
Taz Uddin <getleton@gmail.com>
2020-08-18 00:35:40 +0600

    Add Model

---
+++ SpecificationPattern/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using SpecificationPattern.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecificationPattern.Models
+{
+    public class Movie 
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime RealeaseDate { get; set; }
+        public int RunningLength { get; set; }
+        public int BudgetInMills { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public MpaaRating MpaaRating { get; set; }
+
+        public Movie()
+        {
+
+        }
+
+
+    }
+}
