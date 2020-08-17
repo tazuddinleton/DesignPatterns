a6d298f0edfe452e0c18a3d974e61a657d9133bf
Taz Uddin <getleton@gmail.com>
2020-08-18 00:36:15 +0600

    Configure DbContext

---
+++ SpecificationPattern/Persistence/MoviesConfigurations.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using SpecificationPattern.Models;
+using SpecificationPattern.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecificationPattern.Persistence
+{
+    public class MoviesConfigurations : IEntityTypeConfiguration<Movie>
+    {
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+
+            builder.Property(c => c.RealeaseDate)
+            .HasColumnType("datetime");
+
+            builder.Property(x => x.MpaaRating)
+                .HasConversion<int>();
+
+
+            for (int i = 1; i < 1001; i++)
+            {
+                int mpaa = new Random().Next(4);
+                double rating = new Random().NextDouble() * (10 - 1) + 1;
+
+                var start = new DateTime(1950, 1, 1);
+                var range = (DateTime.Today - start).Days;
+
+                var random = new Random();
+                DateTime releaseDate = start.AddDays(random.Next(range));
+
+                var runTime = new Random(50).Next(210);
+                builder.HasData(new Movie()
+                {
+                    Id = i,
+                    Title = $"Movie {i}",
+                    Director = $"Director {i % 20}",
+                    BudgetInMills = i % 100,
+                    Genre = $"Genre {i % 12}",
+                    MpaaRating = (MpaaRating)mpaa,
+                    Rating = rating,
+                    RealeaseDate = releaseDate,
+                    RunningLength = runTime
+                });
+            }
+
+        }
+    }
+}
---
+++ SpecificationPattern/Persistence/MoviesContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpecificationPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificationPattern.Persistence
+{
+    public class MoviesContext : DbContext
+    {
+        private readonly string _connString = "Server = (localdb)\\mssqllocaldb; Database=Movies;";
+
+        public DbSet<Movie> Movies { get; set; }
+
+
+        public MoviesContext()
+        {
+
+        }
+
+        public MoviesContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MoviesContext).Assembly);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connString);
+        }
+
+    }
+}
