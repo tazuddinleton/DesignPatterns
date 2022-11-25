8b819a288cea712851522d7a552dc59adc0a716b
Taz Uddin <getleton@gmail.com>
2022-11-25 23:51:53 +0600

    Move to seperate file

--- ChainOfResponsibilityPattern/PaymentProcessing/Entities/Item.cs
+++ ChainOfResponsibilityPattern/PaymentProcessing/Entities/Item.cs
@@ -1,4 +1,3 @@
 namespace ChainOfResponsibilityPattern.PaymentProcessing;
 
 public record class Item(string name, string description, decimal price);
-public record class LineItem(Item item, int quantity);
---
+++ ChainOfResponsibilityPattern/PaymentProcessing/Entities/LineItem.cs
@@ -0,0 +1,3 @@
+namespace ChainOfResponsibilityPattern.PaymentProcessing;
+
+public record class LineItem(Item item, int quantity);
